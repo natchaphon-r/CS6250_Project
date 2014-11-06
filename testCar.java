a5cf9de094ae034bd05f37a274199cfcf72e8a87
Adrian <adriangg2961@gmail.com>
2014-11-06 15:43:19 -0500

    Update testCar.java
    - Created the following classes with their respective properties:  event{ ID, position, timeStamp, broadcast}, eventCreated {farthestVID, longestDistance, vIDsHeard, event} , EWMmessage{ senderPosition, senderDirection, senderVID, event}
    - I've added a lot of code to both the EWM and ACK functions.

--- testCar.java
+++ testCar.java
@@ -1,4 +1,9 @@
-/* Pseudo code 1
+/*
+EWM is used for security
+warning, which contains the sender’s position and ID and direction of travel, the ID 
+and location of the event and event timestamp, and message lifetime.
+*/
+/* Pseudo code 1 comments
 //process of broadcast-vehicle-choosing. 
 After broadcast an EWM message with an event-id, 
 wait for period t to receive any messages from behind;
@@ -19,7 +24,7 @@ If (duration<t){
 Else {choose the Vehicle and send ACK; stop broadcast ;  ** THIS IS THE BROADCASTAWK() FUNCTION **  }  */
 
 /*
-// pseudo code 2 
+// pseudo code 2 comments
 If (A vehicle received any messages) { 
 	If (messages==EWM) {  ** THIS IS THE LISTENEWM() FUNCTION **  
 		If (the event-id is the same) {discard ;} 
@@ -38,17 +43,57 @@ If (A vehicle received any messages) {
 
 import java.io.*; 
 import java.net.*; 
-class eventId{
-  String farthestVid;
-  int longestDistance;
-  String vIdsHeard = [];
-  boolean broadcast;
+
+class event {
+	String ID;
+	int position;
+	String timeStamp;
+	boolean broadcast;
+	
+	public event(String eID, int pos, String stamp, boolean broadcastIt) {
+		ID = eID; 
+		position = pos;
+		timeStamp = stamp;
+		broadcast = broadcastIt;
+	}
+	
+}
+
+class eventCreated{
+	String farthestVID = "";
+	int longestDistance = 0;
+	ArrayList<String> vIDsHeard = new ArrayList<String>();
+	event eventDetails;
+	public eventCreated(event e) {
+		eventDetails = e;
+	} 
 } 
+
+class EWMmessage {
+	int senderPosition;
+	int senderDirection;
+	String senderVID;
+	event e;
+	public EWMMessage(int sendPos, int SendDir, String Vid, event ev) {
+		senderPosition = sendPos;
+		senderDirection = SendDir;
+		senderVID = Vid;
+		e = ev;
+	}
+}
   
 class testCar { 
-  boolean isDangerous = False;
-  eventIds = new HashMap<String, eventId>();
-  String vehicleID = "";
+	boolean isDangerous = False;
+	eventIDListening = new HashMap<String, eventCreated>(); //Stores all the eventCreated by this car. AKA all the events created by this car
+	eventsHeard = new HashMap<String, event>; //Stores all the events that this car has heard from the simulator. Key = eventID
+	String vehicleID = "";
+	int position; //prob won't be an int
+	int direction; //prob won't be an int
+	
+	int delay = 1; //1ms delay for listening to EWM responses 
+	int listenedTime = 0;
+	int duration = 20;  // keep listening for responses for 20 milliseconds
+
   
     public void requestSim(String options){
       // options
@@ -63,69 +108,74 @@ class testCar {
       }
       if options = create_EWM{ // only dangerous car, need simulator to keep track of all event ids
         // parameter = vehicleID
-        // simulator assigns characteristic of an event .....
-        set isDangerous to true and eventId.broadcast to true for the given eventID
+        // simulator assigns characteristic of an event (ID, position of event, timestamp)
+        //set isDangerous to true
+		//Add the event infto to the vehicle's eventIDs HasMap and set eventID.broadcast to true for the given eventID
       }
       if options = carReq { // request for car info to calculate distance to it 
-        // parameter = vehicle ID
+        // parameter = this.vehicleID
       }
 
     }
 
 	
-	public void broadcastEWM(String eventId){ 
+	public void broadcastEWM(event e){ 
 		//This broadcast is simple, there is no time to wait for response, no determining longest distance.
 		//This is just simply a broadcast..that's it
-		simulatorBroadcast("EMW", eventId, vehicleId);
+		//contains the sender’s position and ID and direction of travel, the ID and location of the event and event timestamp, 
+		//and message lifetime.
+		EWMmessage message = new EWMmessage(this.position, this.direction, this.vehicleID, e);
+		simulatorEMWBroadcast(message);
 	}
 	
 	
-    public void primaryBroadcastEWM(){
-		// tell simulator it's sending to everyone and simulator will decide who will receive the broadcast
-/*
-** THIS IS THE PRIMARYBROADCASTEWM() FUNCTION**
-If (duration<t){ 
-	If (received any EWMs from behind  ** THIS IS THE LISTENEWM_RESPONSE() FUNCTION**){   
-		if (event-id is the same) {
-			if (Vid was received before)  {Ignore message}; 
-			Else {
-				record the Vid into the queue and calculate the distance. If distance > prev greatest distance then update the Vid and distance;
+    public void primaryBroadcastEWM(event e){
+		EWMmessage message = new EWMmessage(this.position, this.direction, this.vehicleID, e);
+		simulatorEMWBroadcast(message);
+		//First we create the new action listener object. 
+		ActionListener actionToPerform = new ActionListener() {  
+			//Then we create the method that is called each time the action listener is called.
+			public void actionPerformed(ActionEvent evt) { 
+				//...Perform your task here, whatever it may be.
+					if(listenedTime < duration){
+						listenedTime++;
+						listenEWM_Response(e);
+					}
 			}
-		}
-		Else {broadcastEWM() with this event-id ;}
+		};
+		new Timer(delay, actionToPreform).start();
+			
 	} 
-	Else {after broadcast an EWM, still wait for a period t ;}
-} 
-
-Else {choose the Vehicle and send ACK; stop broadcast ;  ** THIS IS THE BROADCASTAWK() FUNCTION **  }  */
-
-
-
-    }
 	
-    public void broadcastACK(String eventId){
-      //calculate the vehicleID that's furthest away
-	  if(eventIds.containsKey(eventId)) {
-		eventIds.get(eventId).broadcast = false; 
-		simulatorBroadcast("AWK", eventIds.get(eventId).farthestVid, eventId);
-	  }
-	  else { System.out.println("Attempted to Broadcast an ACK for event ID " + eventID + ". But wasn't found in the event list"); }
+    public void broadcastACK(event e){
+		//calculate the vehicleID that's furthest away
+		if(eventIDListening.containsKey(e.eventID)) {
+			eventIDListening.get(e.eventID).broadcast = false; 
+			simulatorAWKBroadcast(eventIDListening.get(e.eventID).farthestVid, e);
+		}
+		else { System.out.println("Attempted to Broadcast an ACK for event ID " + eventID + ". But wasn't found in the event list"); }
 	}
 	  
     public void listenEWM(){
-	/* //Every car should always be listening for EWMs...always
-	
-	If (messages==EWM) {   
-		If (the event-id is the same) {discard ;} 
-		Else broadcastEWM() ;
-	} 
-	*/
+		/*
+		A vehicle ignores an EWM if it comes from behind with respect to its travel direction, but can infer that there is an 
+		emergency event ahead when an EWM comes from the front, and immediately decelerates and broadcasts EWM of its own.
+		 
 
+			If (the event-id is the same) {discard ;} 
+			Else broadcastEWM() ;
+		*/
+		// Assume that the simulator will return an arrayList with EWM messages called RecievedEWM
+		for(EWMmessage : RecievedEWM) {
+			if(!isBehind(this.position, EWMmessage.senderPosition) && movingSameDirection(this.direction, EWMmessage.senderDirection) && !eventsHeard.containsKey(EWMmessage.e.eventID)) {
+				eventsHeard.put(EWMmessage.e.eID, EWMmessage.e);
+				broadcastEWM(EWMmessage.e);
+			}
+		}
     }
     
-	public void listenEWM_Response() {
-	
-/* If (duration<t){ 
+	public void listenEWM_Response(event e) {
+	/*
 	If (received any EWMs from behind  **THIS IS THE LISTENEWM_RESPONSE() FUNCTION   ){     
 		if (event-id is the same) {
 			if (Vid was received before)  {Ignore message}; 
@@ -133,13 +183,21 @@ Else {choose the Vehicle and send ACK; stop broadcast ;  ** THIS IS THE BROADCAS
 				record the Vid into the queue and calculate the distance. If distance > prev greatest distance then update the Vid and distance;
 			}
 		}
-		Else {  broadcastEWM(this new heard event-id)  ;}
+		Else {  broadcastEWM(this new heard event-id)  ;} //this is taken care of by the listenEWM() function
 	}  */
-	
-	     if EWM comes from a car behind you or traveling in the opposite direction
-        drop it 
-        else broadcastEWM(eventID)
-		
+	// assume that we can get an array of EWM messages from the simulator called RecievedEWM
+		for(EWMmessage : RecievedEWM) {
+			if(isBehind(this.position, EWMmessage.senderPosition) && eventIDListening.contains(EWMmessage.e.eID))  {
+				if(!eventIDListening.get(EWMmessage.e.eID).vIDsHeard.contains(EWMmessage.senderVid)) {
+					eventIDListening.get(EWMmessage.e.eID).vIDsHeard.put(EWMmessage.senderVid);
+					int distance = getDistance(EWMmessage.senderPosition, this.position);
+					if(distance > eventIDListening.get(EWMmessage.e.eID).longestDistance) {
+						eventIDListening.get(EWMmessage.e.eID).longestDistance = distance;
+						eventIDListening.get(EWMmessage.e.eID).farthestVid = EWMmessage.senderVid;
+					}
+				}
+			}
+		}
 	}
 	
 	public void listenACK(){
@@ -151,11 +209,11 @@ Else {choose the Vehicle and send ACK; stop broadcast ;  ** THIS IS THE BROADCAS
 		}
 		Else stop broadcast ;
 	 */
-		if(vehicleId == heardId) {
-			PrimaryBroadcastEWM(heard_eventId);
+		if(vehicleID == heardID) {
+			primaryBroadcastEWM(heardEvent);
 		}
 		else {
-			eventIds[heard_eventId].broadcast = false;
+			eventHeard.get(heardEvent.eID).broadcast = false;
 		}
 	}
 }
