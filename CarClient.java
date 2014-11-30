28f7b375a3f6734b67ac3554132db1c291e20e7a
Natchaphon Ruengsakulrach <nruengsa@gatech.edu>
2014-11-30 18:54:03 -0500

    ip wlan0 working

--- CarClient.java
+++ CarClient.java
@@ -1,5 +1,7 @@
 import java.io.*;
 import java.net.*;
+import java.util.*;
+import static java.lang.System.out;
 
 
 public class CarClient{
@@ -11,23 +13,33 @@ public class CarClient{
 
 	public void registerSim(int port){
 		try{
-		DatagramSocket carSocket = new DatagramSocket(port);
-		
-		InetAddress simaddr = InetAddress.getByName("192.168.173.1");
-		
-		int simport = 50000;
+			DatagramSocket carSocket = new DatagramSocket(port);
+			InetAddress simaddr = InetAddress.getByName("192.168.173.1");
+			int simport = 50000;
+			String strIP = "";
 
-		NetworkInterface ni = NetworkInterface.getByName("wlan0");
-		Enumeration<InetAddress> inetAddresses =  ni.getInetAddresses();
-
-		while(inetAddresses.hasMoreElements()) {
-            InetAddress ia = inetAddresses.nextElement();
-            if(!ia.isLinkLocalAddress()) {
-                System.out.println("IP: " + ia.getHostAddress());
+			// This finds the ip address of interface wlan0.
+			Enumeration<NetworkInterface> nets = NetworkInterface.getNetworkInterfaces();
+        	for (NetworkInterface netint : Collections.list(nets))
+            	if (new String (netint.getDisplayName()).equals("wlan0")){
+            		Enumeration<InetAddress> inetAddresses = netint.getInetAddresses();
+            		/*Set counter, usually display MAC addr first then IP addr.
+            	  	We want the IP address (i = 1)*/
+            		int i = 0;  
+        			for (InetAddress inetAddress : Collections.list(inetAddresses)) {	
+            			//out.printf("InetAddress: %s\n", inetAddress);
+            			if (i == 1){ 
+            				//This is the IP adress
+            				InetAddress caraddr = inetAddress;
+            				strIP = (caraddr.toString()).substring(1);
+            				//System.out.println("wlan0 ip is or strIP is " + strIP);
+            		}
+            		i++;
+        		}
             }
-        }
 
-		InetAddress caraddr = InetAddress.getLocalHost();
+
+		//InetAddress caraddr = InetAddress.getLocalHost();
 
 
 		byte[] receiveData = new byte[1024];
@@ -39,11 +51,12 @@ public class CarClient{
 		request.append(port);
 		*/
 
-		String strIP = caraddr.getHostAddress();
+		//String strIP = caraddr.getHostAddress();
 		String strPORT = Integer.toString(port);
 
 		//Simulator Addr: 192.168.173.1 Port: 50000
 		String reqMSG = "192.168.173.1:50000,"+ strIP + ":" + strPORT;
+		//String reqMSG = "000";
 		sendData = reqMSG.getBytes();
 
 		System.out.println("Sending to Simulator");
@@ -63,14 +76,24 @@ public class CarClient{
 
 
 
-
-
-
-
-
 	}
-	public static void main(String[] args){
+
+	static void displayInterfaceInformation(NetworkInterface netint) throws SocketException {
+        out.printf("Display name: %s\n", netint.getDisplayName());
+        out.printf("Name: %s\n", netint.getName());
+        Enumeration<InetAddress> inetAddresses = netint.getInetAddresses();
+        for (InetAddress inetAddress : Collections.list(inetAddresses)) {
+            out.printf("InetAddress: %s\n", inetAddress);
+        }
+        out.printf("\n");
+     }
+
+	public static void main(String[] args) throws SocketException{
 		CarClient client = new CarClient();
 		client.registerSim(9000);
+		/*
+		Enumeration<NetworkInterface> nets = NetworkInterface.getNetworkInterfaces();
+        for (NetworkInterface netint : Collections.list(nets))
+            displayInterfaceInformation(netint);*/
 	}
 }
