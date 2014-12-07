5c637816d221f6ed806d3db1b101cb25137ab408
Michael <mikesroberts@hotmail.com>
2014-12-06 19:50:50 -0600

    Update CarClient.java

--- CarClient.java
+++ CarClient.java
@@ -12,43 +12,18 @@ public class CarClient{
 	messageListener listener;
 	DatagramSocket sock = new DatagramSocket();
 	private int port;
+	private String strIP = "127.0.0.1"; //<--192.168.173.220";
 
-	public void registerSim(int port){
+	public void registerSim(){
 		try{
-			//set up listening stuff
-			listener = new messageListener();
-			listener.start();
 			
-			this.port = port;
-			DatagramSocket carSocket = new DatagramSocket(port);
-			InetAddress simaddr = InetAddress.getByName("192.168.173.1");
+			DatagramSocket carSocket = new DatagramSocket();
+			InetAddress simaddr = InetAddress.getByName("127.0.0.1"); //<--192.168.173.220
 			int simport = 50000;
-			String strIP = "";
-
-			// This finds the ip address of interface wlan0.
-			Enumeration<NetworkInterface> nets = NetworkInterface.getNetworkInterfaces();
-        	for (NetworkInterface netint : Collections.list(nets))
-            	if (new String (netint.getDisplayName()).equals("wlan0")){
-            		Enumeration<InetAddress> inetAddresses = netint.getInetAddresses();
-            		/*Set counter, usually display MAC addr first then IP addr.
-            	  	We want the IP address (i = 1)*/
-            		int i = 0;  
-        			for (InetAddress inetAddress : Collections.list(inetAddresses)) {	
-            			//out.printf("InetAddress: %s\n", inetAddress);
-            			if (i == 1){ 
-            				//This is the IP adress
-            				InetAddress caraddr = inetAddress;
-            				strIP = (caraddr.toString()).substring(1);
-            				//System.out.println("wlan0 ip is or strIP is " + strIP);
-            		}
-            		i++;
-        		}
-            }
-		//InetAddress caraddr = InetAddress.getLocalHost();
 
 
-		byte[] receiveData = new byte[1024];
-		byte[] sendData = new byte[1024];
+		
+		byte[] sendData;
 		/*
 		StringBuilder request = new StringBuilder();
 		request.append(ipaddr);
@@ -60,82 +35,113 @@ public class CarClient{
 		String strPORT = Integer.toString(port);
 
 		//Simulator Addr: 192.168.173.1 Port: 50000
-		String reqMSG = "192.168.173.1:50000,"+ strIP + ":" + strPORT;
+		String reqMSG = "127.0.0.1:50000,"+ strIP + ":" + strPORT + ",register" + ",REGmsg"; //<-- 192/168.173.1:50000
 		//String reqMSG = "000";
 		sendData = reqMSG.getBytes();
 
-		System.out.println("Sending to Simulator");
+		System.out.println("REGISTERSIM: Sending to Simulator");
 		DatagramPacket sendRequest = new DatagramPacket(sendData, sendData.length, simaddr, simport);
+		System.out.println("Registration request MSG = "+new String(sendData));
 		carSocket.send(sendRequest);
 
+		carSocket.close();
+		/*
 		DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
 		carSocket.receive(receivePacket);
 		String fromSim = new String(receivePacket.getData());
 		System.out.println("FROM SIMULATOR:" + fromSim);
+		System.out.println("---------------------------------------------");
 
 		carSocket.close();
+		*/
+		
 		}catch(IOException e1){
 			System.out.println("UnknownHostError?");
 			e1.printStackTrace();
 		}
-
 	}
 	
-	public void sendMSG(String message){
+	
+	public void requestPOSDIR(){
 		try{
 			DatagramSocket carSocket = new DatagramSocket();
-			InetAddress simaddr = InetAddress.getByName("192.168.173.1");
+			InetAddress simaddr = InetAddress.getByName("127.0.0.1");//<--192.168.173.1
 			int simport = 50000;
-			String strIP = "192.168.173.220";
+			byte[] receiveData = new byte[1024];
+			byte[] sendData = new byte[1024];
+			String strPORT = Integer.toString(port);
+			
+			//Create message addressed to simulator
+			String reqMSG = "192.168.173.1:50000,"+ strIP + ":" + strPORT + ",1" + ",randomMessage";
+			sendData = reqMSG.getBytes();
 
-		
+			System.out.println("SENDING pos/dir REQUEST: Sending to Simulator");
+			DatagramPacket sendRequest = new DatagramPacket(sendData, sendData.length, simaddr, simport);
+			carSocket.send(sendRequest);
 
-		byte[] receiveData = new byte[1024];
-		byte[] sendData = new byte[1024];
-		/*
-		StringBuilder request = new StringBuilder();
-		request.append(ipaddr);
-		request.append(":");
-		request.append(port);
-		*/
-
-		//String strIP = caraddr.getHostAddress();
-		String strPORT = Integer.toString(port);
-
-		//Simulator Addr: 192.168.173.1 Port: 50000
-		//message should be of form (type,paramaters)
-		String reqMSG = "255.255.255.0:7000,"+ strIP + ":" + strPORT + "," + message;
-		//String reqMSG = "000";
-		sendData = reqMSG.getBytes();
-
-		System.out.println("Sending to Simulator");
-		DatagramPacket sendRequest = new DatagramPacket(sendData, sendData.length, simaddr, simport);
-		carSocket.send(sendRequest);
-
-		carSocket.close();
+			
+			//should'nt need cause car is already listening
+			/*
+			DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+			carSocket.receive(receivePacket);
+			String fromSim = new String(receivePacket.getData());
+			System.out.println("requestPOSDIR gets CONTAINS:" + fromSim);
+			System.out.println("---------------------------------------------");
+			 */
+			carSocket.close();
+			
+			
 		}catch(IOException e1){
 			System.out.println("UnknownHostError?");
 			e1.printStackTrace();
 		}
+	}
+	
+	
+	public void sendMSG(String msgType,String message){
+		try{
+			DatagramSocket carSocket = new DatagramSocket();
+			InetAddress simaddr = InetAddress.getByName("127.0.0.1"); //<-- 192.168.173.1
+			int simport = 50000;
 
+			byte[] receiveData = new byte[1024];
+			byte[] sendData = new byte[1024];
+			String strPORT = Integer.toString(port);
+
+			//Simulator Addr: 192.168.173.1 Port: 50000
+			//message should be of form (type,paramaters)
+			String reqMSG = "255.255.255.0:7000,"+ strIP + ":" + strPORT + "," + msgType + "," + message;
+			sendData = reqMSG.getBytes();
+
+			System.out.println("Sending to Simulator from sendMSG func");
+			DatagramPacket sendRequest = new DatagramPacket(sendData, sendData.length, simaddr, simport);
+			carSocket.send(sendRequest);
+			carSocket.close();
+
+		}catch(IOException e1){
+			System.out.println("UnknownHostError?");
+			e1.printStackTrace();
+		}
 	}
 	
 	private class messageListener extends Thread{
-
 		DatagramSocket sock;
-		DatagramPacket pack;
-		byte[] buf = new byte[256];
-
+		String currentEWM[]={"","","","","","","",""}; //type,xpos,ypos,xdir,ydir,ack_car_id,event_id,event_ttl
+		boolean EWMComplete = false;
+		
 		public messageListener() throws IOException
 		{
-			super("message listener");
+			super("MESSAGELISTENER: STARTING THE THREAD");
 			sock = new DatagramSocket(port);
 		}
-
 		public void run()
 		{
+			System.out.println("INSIDE THREAD.RUN: Start listening");
+			
 			while(true)
 			{
+				DatagramPacket pack;
+				byte[] buf = new byte[256];
 				pack = new DatagramPacket(buf, buf.length);
 				try {
 					sock.receive(pack);
@@ -144,15 +150,104 @@ public class CarClient{
 					e1.printStackTrace();
 				}
 				String msg = (new String(pack.getData())).trim();
-				System.out.println("Got message:" + msg) ;
-			}
+				System.out.println(port+"MESSAGELISTENER:Got message:" + msg);
+				String arrayMSG[] = msg.split(",");
 
-		}
+				String msgType = arrayMSG[2];
+				
+				//if it's an EWM message
+				//Extract EWM msg info to propogate and request pos/dir from simulator
+				if (msgType.equals("1")){	
+					//Capture relevant parts of the message
+					currentEWM[0] = arrayMSG[2];//message type
+					currentEWM[5] =	arrayMSG[7];//ack_car_id
+					currentEWM[6] =	arrayMSG[8];//event_id
+					currentEWM[7] =	arrayMSG[9];//event_ttl
+					
+					//request xpos,ypos,xdir,ydir
+					System.out.println("NOW REQUESTING POSITION INFO");
+					requestPOSDIR();
+					
+				}
+				
+				//if it's a response to requestPOSDIR (or a registration message response)
+				//insert xpos,ypos etc values into the EWM message being built
+				if(msgType.equals("info")){
+					currentEWM[1] = arrayMSG[3]; //x-pos
+					currentEWM[2] = arrayMSG[4]; //y-pos
+					currentEWM[3] = arrayMSG[5]; //currently direction (should just make this direction and modify Adrian func?)
+					currentEWM[4] = arrayMSG[6]; //currently diff in y coordinate
+				}
+				
+				//Check if EWM is finished being built
+				for (int i=0; i<currentEWM.length; i++){
+					if (currentEWM[i]==""){
+						EWMComplete=false;
+						break;
+					}else
+						EWMComplete=true;
+				}
+				
+				//If EWM is built, send to Adrian code? Broadcast?
+				if(EWMComplete){
+					System.out.println(port+"Ready to broadcast: "+currentEWM[0]+", "+currentEWM[1]+", "
+							+currentEWM[2]+", "+currentEWM[3]+", "+currentEWM[4]+", "+currentEWM[5]+", "
+							+currentEWM[6]+", "+currentEWM[7]+", ");
+					
+					EWMComplete=false;
+					//clear EWM here?
+				}
+				
+				//.info denotes the message originated at the simulator
+				//if (!(msgType.equals("info"))){
+				//requestPOSDIR();
+				//}
+				
+				/*
+				//if message was message was a response to a request for information..
+				if ((msgType.equals("info"))){
+					String sender_pos_x = arrayMSG[3];
+					String sender_pos_y = arrayMSG[4];
+				  //String sender_dir_x = arrayMSG[];
+					String sender_dir = arrayMSG[5];
+					String send_id = strIP+":"+port;
+					String ack_car_id;
+					String event_id;
+					String event_ttl;
+					
+					}
+				}*/
+				
+				/*
+				String posdir[] = requestPORDIR();
+				String sender_pos_x = posdir[0];
+				String sender_pos_y = posdir[1];
+				String sender_dir_x = posdir[2];
+				String sender_dir_y = posdir[3];
+				String send_id = strIP;
+				String ack_car_id;
+				String event_id;
+				String event_ttl;
+				*/
+				
+				/* Type 1 = EWM
+				   Type 2 = ACK*/
+				
+				//if (msgType.equal("1")){
+				//	reactEWM(arrayMSG[2],arrayMSG[3])	
+				}
+			}
+		
 	}
-	public CarClient(int portNo) throws IOException{
+	
+	public CarClient(int portNo) throws IOException {
 		this.port = portNo;
+		this.registerSim();
+		
+		System.out.println("Creating a listener");
 		listener = new messageListener();
 		listener.start();
+
 	}
 	
 	static void displayInterfaceInformation(NetworkInterface netint) throws SocketException {
@@ -167,40 +262,16 @@ public class CarClient{
 
 	public static void main(String[] args) throws SocketException,IOException{
 		CarClient client1 = new CarClient(9000);
-		CarClient client2 = new CarClient(9001);
+		
+		CarClient client2 = new CarClient(9001);	
 		CarClient client3 = new CarClient(9002);
 		CarClient client4 = new CarClient(9003);
 		CarClient client5 = new CarClient(9004);
 		CarClient client6 = new CarClient(9005);
-		
-		//send a message from each car to sim
-		client1.sendMSG("1,register1");
-		client2.sendMSG("1,register2");
-		client3.sendMSG("1,register3");
-		client4.sendMSG("1,register4");
-		client5.sendMSG("1,register5");
-		client6.sendMSG("1,register6");
-		
-		try{
-		Thread.sleep(20);
-		}catch(InterruptedException ie){
-			System.out.println("sleep messed up in main");
-		}
-		
-		//this should broadcast collision ahead to any node within range of client 3
-		client3.sendMSG("1,collisionahead");
-		try{
-			Thread.sleep(20);
-			}catch(InterruptedException ie){
-				System.out.println("sleep messed up in main");
-			}
-		
-		//brodcast an ACK to any node in range of client 2
-		client2.sendMSG("2,this is an ACK");
-		
-		/*
-		Enumeration<NetworkInterface> nets = NetworkInterface.getNetworkInterfaces();
-        for (NetworkInterface netint : Collections.list(nets))
-            displayInterfaceInformation(netint);*/
+
+		//type is not included and will have to be inserted during send
+		//EMW = sender_x_pos, sender_y_pos, sender_dir_x, sender_dir_y, ack_car_id, event_id,event_ttl 
+		String EWM_msg = "500.12,300.41,700.0,900.0,ack_car_id,1,20";
+		client1.sendMSG("1", EWM_msg);
 	}
 }
